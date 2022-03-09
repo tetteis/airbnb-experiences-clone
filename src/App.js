13a7288ebefc6085a7f05a1aed775616e519e1c0
Tettei Shahday-Annang <tetteis@gmail.com>
2022-03-09 11:47:07 +0000

    Modified App, Card and style

--- src/App.js
+++ src/App.js
@@ -6,25 +6,14 @@ import data from "./data";
 import "./style.css";
 
 export default function App() {
-  const dataCard = data.map((item) => {
-    console.log(item);
-    return (
-      <Card
-        key={item.id}
-        img={item.coverImg}
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-      />
-    );
+  const cards = data.map((item) => {
+    return <Card key={item.id} item={item} />;
   });
   return (
     <div>
       <Navbar />
       <Hero />
-      {dataCard}
+      <section className="cards-list">{cards}</section>
     </div>
   );
 }
